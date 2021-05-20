896e8017cd9044062578c01a08c6f555bc952957
DanubiaM <dubamacedoo@gmail.com>
2021-05-20 19:48:58 -0400

    adicionado link para acesso a icons

--- src/main/java/edu/ifmt/cobrancaifmt/CadastroTituloController.java
+++ src/main/java/edu/ifmt/cobrancaifmt/CadastroTituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller 
 @RequestMapping("/titulos") //tras o mapeamento /titulos para todo o controller
@@ -32,16 +33,14 @@ public class CadastroTituloController {
 	private Titulos titulos;
 
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) { 		//@validated é usada para que possa ser valida pelas regras criada no modelo
-																				//Error captura os erros existentes e tras uma mensagem sobre o erro
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) { //@validated é usada para que possa ser valida pelas regras criada no modelo
+																								//Error captura os erros existentes e tras uma mensagem sobre o erro		
 		if(errors.hasErrors()) {
-			return mv;
-		}
+			return "cadastroTitulo";															//RedirectAttributee envia atributos para o redirect
+		}																						//apresentado no return "redirect:/titulos/novo"
 		titulos.save(titulo);
-		mv.addObject("mensagem", "Foi Salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Foi salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
