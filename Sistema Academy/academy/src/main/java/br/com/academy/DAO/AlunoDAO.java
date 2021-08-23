21bd0ce3ad5bbbbaefe9dab87ef188e6f4f637b3
DanubiaM <dubamacedoo@gmail.com>
2021-08-23 15:23:28 -0400

    feat: adding query status in JPA

--- Sistema Academy/academy/src/main/java/br/com/academy/DAO/AlunoDAO.java
+++ Sistema Academy/academy/src/main/java/br/com/academy/DAO/AlunoDAO.java
@@ -1,10 +1,16 @@
 package br.com.academy.DAO;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.academy.model.Aluno;
 
 // JpaRepository<Aluno, Integer> 'Aluno' é o nome da classe que sera utilzada para criar um db, e Integer é tipo do atributo identificado
 public interface AlunoDAO extends JpaRepository<Aluno, Integer> {
 
+	@Query("Select resultado from Aluno resultado where j.status = 'ATIVO' ")
+	public List<Aluno> findByStatusAtivos();
+	
 }
