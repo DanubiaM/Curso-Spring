2cd637cdbc9e0e5956da45b3e4fef3febf856b00
DanubiaM <dubamacedoo@gmail.com>
2021-08-25 11:19:58 -0400

    feat: added query from login

--- Sistema Academy/academy/src/main/java/br/com/academy/DAO/UsuarioDAO.java
+++ Sistema Academy/academy/src/main/java/br/com/academy/DAO/UsuarioDAO.java
@@ -10,4 +10,7 @@ import br.com.academy.model.Usuario;
 public interface UsuarioDAO extends JpaRepository <Usuario, Long> {
 	@Query("select resultado from Usuario resultado where resultado.email = :email")
 	public Usuario findByEmail(String email);
+	
+	@Query("select resultado from Usuario resultado where resultado.user = :user and resultado.senha = :senha")
+	public Usuario buscarLogin(String user, String senha);
 }
