113f308c9272c4d74b3c73a4731d770e8de3ff19
DanubiaM <dubamacedoo@gmail.com>
2021-08-25 11:32:05 -0400

    feat: added exception

---
+++ Sistema Academy/academy/src/main/java/br/com/academy/exceptions/ServiceExcp.java
@@ -0,0 +1,12 @@
+package br.com.academy.exceptions;
+
+public class ServiceExcp extends Exception {
+
+	public ServiceExcp(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final Long serialVersionUID = 1L;
+
+}
